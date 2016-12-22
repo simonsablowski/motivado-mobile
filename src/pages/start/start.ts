2161bfa70e8115f4bed339b0dfc695d49ed1bfb2
Simon Sablowski <contact@simsab.net>
2016-12-22 11:07:38 +0100

    loading featured products on start page
    loading featured products from local json file on start page, improving
    product page’s layout, adding required components to start.ts

--- src/pages/start/start.ts
+++ src/pages/start/start.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
 import { CoachingProductService } from '../../providers/coaching-product-service';
 import { CoachingProductListPage } from '../coaching-product-list/coaching-product-list';
 import { CoachingProductPage } from '../coaching-product/coaching-product';
@@ -9,40 +10,26 @@ import { CoachingProductPage } from '../coaching-product/coaching-product';
 	providers: [ CoachingProductService ]
 })
 export class StartPage {
-  products: Array<{title: string, description: string, image: string, component: any}>;
+  products: Array<{ title: string, description: string, image: string, component: any }>;
 
   constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
     public coachingProductService: CoachingProductService
   ) {
     this.loadFeaturedProducts();
   }
 
   loadFeaturedProducts() {
-    /*this.coachingProductService.loadFeaturedProducts()
+    this.coachingProductService.loadFeaturedProducts()
     .then(data => {
       this.products = data;
-    });*/
+    });
+  }
 
-    //TODO: use CoachingProductService and featured flag
-    this.products = [
-      {
-        title: 'Ex zurückgewinnen',
-        description: 'Erstellen Sie mit unseren Experten Ihre persönliche Ex-Zurück-Strategie.',
-        image: 'assets/img/slide-exzurueck.jpg',
-        component: CoachingProductPage
-      },
-      {
-        title: 'Endlich abnehmen',
-        description: 'Abnehmen ist zu 85% Kopfsache! Jetzt bis zu 0,5kg pro Woche zusätzlich abnehmen.',
-        image: 'assets/img/slide-abnehmen.jpg',
-        component: CoachingProductPage
-      },
-      {
-        title: 'Jedes 3. Kind wird gemobbt!',
-        description: 'TV-Experte Thomas Sonnenburg hilft Ihnen richtig gegen Mobbing vorzugehen.',
-        image: 'assets/img/slide-mobbing.jpg',
-        component: CoachingProductPage
-      }
-    ];
+  selectProduct(event, product) {
+    this.navCtrl.push(CoachingProductPage, {
+      product: product
+    });
   }
 }
